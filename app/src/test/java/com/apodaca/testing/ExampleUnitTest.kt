8b6ac6ca8dd32b700e5d2cc08419544b5b9a336e
Apodaca <eduardo1ja99@gmail.com>
2023-09-05 21:51:47 -0600

    test: test basic

--- app/src/test/java/com/apodaca/testing/ExampleUnitTest.kt
+++ app/src/test/java/com/apodaca/testing/ExampleUnitTest.kt
@@ -1,8 +1,11 @@
 package com.apodaca.testing
 
+import assertk.assertThat
+import assertk.assertions.isEqualTo
 import org.junit.Test
+import org.junit.jupiter.api.AfterEach
+import org.junit.jupiter.api.BeforeEach
 
-import org.junit.Assert.*
 
 /**
  * Example local unit test, which will execute on the development machine (host).
@@ -10,8 +13,20 @@ import org.junit.Assert.*
  * See [testing documentation](http://d.android.com/tools/testing).
  */
 class ExampleUnitTest {
+
+
+    @BeforeEach
+    fun setUp() {
+
+    }
+
+    @AfterEach
+    fun tearDown() {
+
+    }
+
     @Test
     fun addition_isCorrect() {
-        assertEquals(4, 2 + 2)
+        assertThat(2 + 3).isEqualTo(4)
     }
 }
